27c8e120f8c230fb30d4d837882b27a4cfe1cf93
krzysztof <235060@student.pwr.edu.pl>
2019-05-25 16:04:05 +0200

    Initial Commit

---
+++ stronghold-threads/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stronghold-threads
+//
+//  Created by Krzysztof Kapała on 25/05/2019.
+//  Copyright © 2019 Krzysztof Kapała. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
