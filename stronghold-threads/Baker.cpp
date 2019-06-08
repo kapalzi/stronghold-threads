08e10bd236aff6033d738b9a4d618e85fb406a9b
Grzegorz Tomasik <grzesiek315@gmail.com>
2019-06-08 17:08:12 +0200

    grouping

---
+++ stronghold-threads/Baker.cpp
@@ -0,0 +1,8 @@
+#include "Baker.h"
+
+void Baker::goForResources()
+{
+    
+   Human::goForResources();
+
+}
---
+++ stronghold-threads/Miner.cpp
@@ -0,0 +1,7 @@
+#include "Miner.h"
+#include "Human.h"
+
+void Miner::goForResources()
+{
+    Human:goForResources();
+}
